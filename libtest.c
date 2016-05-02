76bc15bc01a2a06897ef30c5ca3a3766d8ad7591
corbeygh <corbeygh@users.noreply.github.com>
2016-05-02 19:37:43 +1200

    libtest
    fixed errors in code

--- libtest.c
+++ libtest.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 
-\\these load specific methods from the ENGR101 library
+//these load specific methods from the ENGR101 library
 extern "C" int init(int d_lev);
 extern "C" int take_picture();
 extern "C" char get_pixel(int row,int col,int colour);
@@ -14,7 +14,7 @@ int main(){
     char c;
     while(true){
        //Take picture with camera
-       take_picture()
+       take_picture();
        //get pixel "whiteness" from centre of image (160x120)
        c = get_pixel(160,120,3);
        //Prints read pixel value
