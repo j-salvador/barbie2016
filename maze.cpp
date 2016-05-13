e916a31db2f1e33f3293ff0f0d0c699c34a78bb5
Jesse Wood <jessewood0876@gmail.com>
2016-05-13 22:17:18 +1200

    maze.cpp v4 - on a new Editor
    Worked out how to use the Atom text editor with a g++ compiler

--- maze.cpp
+++ maze.cpp
@@ -1,11 +1,12 @@
+ //imports all of the main libraries
 #include <iostream>
-#include <stdio.h>	//import libraries
+#include <stdio.h>
 #include <stdbool.h>
 #include <time.h>
 #include <math.h>
 #include <stdbool.h>
 #include <string.h>
-
+//------------------------------------------------------------------------------
 //these load specific methods from the ENGR101 library
 extern "C" int init(int d_lev);
 extern "C" int take_picture();
@@ -14,89 +15,129 @@ extern "C" int Sleep(int sec, int usec);
 extern "C" int set_motor( int motor , int speed);
 extern "C" int set_PWM( int chan , int value );
 extern "C" char set_pixel(int row, int col,char red,char green, char blue);
-
-//extern "C" int set_PWM( int chan , int value );
-//extern "C" char set_pixel(int row, int col,char red,char green, char blue);
-
+//-----------------------------------------------------------------------------
+//Declaraiton of the functions.
 float takeReading(int analogChannel);
 void setSpeed(int motor, int speed);
-void PWM(int dir, int speed);
-void mTurn(int dir, int setSpeed);
+void PWM(int dir, int motor, int speed);
 void deadEndTurn();
 void mazeCheck();
 void mazePID();
 int main();
-
+//------------------------------------------------------------------------------
 //Initialization of global variables
 float speed = 30; //Sets the speed through out the maze to 30
 float dWall = 50; //Sets the average wall Distance when the robot is sensored, ie dLeft = dRight at facing foward.
 bool inMaze = false; //The AVC doesn't start in the maze, when it detects it is, this will trigger.
 float dWallError = 5; //The acceptable error value, that the PID can handle for dead end turns and correct.
 bool readyPWM = false; //Initially set to false, since it will be at rest.
-float maxSpeed = 50;
-float maxReading = 100;
-float deadSpeed = 40;
+float maxSpeed = 50; //Sets the maximum speed for the AVC going through the maze.
+float maxReading = 100; //This is the maximum reading for the IR sensors when they are in the maze.
+float currSpeed = 0; //A global variables to store the current speed of the AVC for PWM.
 
-float dLeft;
-float dRight;
-
+float deadSpeed = 40; //The speed at which it turns at dead ends.
+float dLeft; //variables to store the readings on the left sensors
+float dRight; //Variables to store the reading on the right sensor.
 //Sets the motors as global variables so they can be changed easily if the order is changed
 int m1 = 1;
 int m2 = 2;
-
+//Stores the sensors as global variables so that their order can be easily changed
 int s1 = 1;
 int s2 = 2;
-
+//These variables simply refer to directions, the numbers are arbitrary, since strings can't be used variables are easier
 int left = -2;
 int right = 2;
 int forward = 1;
 int backward = -1;
 
+float currDir = forward;
+//------------------------------------------------------------------------------
 /**
 * @param anologChannel
 * @method takeReading
 */
 float takeReading(int analogChannel){
-    int sensor;
-    float x;
-    int y;
+    //Initialization of local variables
+    float x; //Float used to convert the reading to a float
+    int y; //Float used to store the reading
+    //If the channel is the left sensor
 	if (analogChannel == s1){
         y = ReadAnalog(s1);
         x = (float)y;
         dLeft = x;
 	}
+    //If the channel is the right sensor
 	if (analogChannel == s2){
-		sensor = s2;
-        y = ReadAnalog(s2);
+    y = ReadAnalog(s2);
 		x = (float) y;
 		dRight = x;
 	}
 	return x; //Might not work in c
 }
-
+//------------------------------------------------------------------------------
 /**
 * @param dir:
-* @param setSpeed
-* @method mTurn
+* @param vel
+* @method PWM
 */
-void PWM(int dir, int vel){
+void PWM(int dir, int motor, int vel){
 	//Handles acceleration and turning
-	int lowerVel;
+    //Initializes the local variables
+    int lowerVel;
 	int higherVel;
-	if (vel )
-	readyPWM = true;
-	return;
+    int PWMspeed;
+    int subtractionConst;
+    //Checks if the desired velocity is larger than the current.
+    if (vel > currentSpeed){
+        lowerVel = currentSpeed;
+        higherVel = vel;
+        PWMspeed = currentSpeed;
+        subtractionConst = 1;
+    }
+    //Checks if the desired velocity is smaller than the current
+    else if (vel < currentSpeed){
+        lowerVel = vel;
+        higherVel = currentSpeed;
+        PWMspeed = currentSpeed;
+        subtractionConst = -1;
+    }
+    //Loops until the current velocity is equal to the desired velocity
+    for (int i = 0; lowerVel < higherVel; lowerVel++){
+        //Checks if the robot is turning left
+        if (dir == left){
+            //Makes the robot turn left
+            set_PWM(s1, PWMspeed);
+            set_PWM(s2, PWMspeed);
+        }
+        //Checks if the robot is turning right
+        if (dir == right){
+            //Makes the robot turn right
+            set_PWM(s1, -1*PWMspeed);
+            set_PWM(s2, -1*PWMspeed);
+        }
+        //Else if the robot is going forward
+        else {
+            //Makes the robot go forward
+            set_PWM(s1, PWMspeed);
+            set_PWM(s2, -1*PWMspeed);
+        }
+        PWMspeed += subtractionConst;
+        Sleep (0 ,010000);
+    }
+	readyPWM = true; //Once the AVC has either accelerated or decelerated to the desired speed the function will stop.
+    setSpeed(3, int PWMspeed);
+    return;
 }
-
+//------------------------------------------------------------------------------
 /**
 * @param motor
-* @param speed
+* @param vel
 * @method setSpeed
 */
 void setSpeed(int motor ,int vel){
-    int dir = 0;
+    int dir = currDir;
 	if (readyPWM == false){ //Checks if the motor is accelerating
-		PWM(dir, vel);	//Calls the PWM funciton if the motor is accelerating
+		PWM(dir, motor, vel);	//Calls the PWM funciton if the motor is accelerating
 	}
 
 	if(motor == m1){
@@ -114,32 +155,10 @@ void setSpeed(int motor ,int vel){
 		set_motor(m2 , -1*vel); //Changes the second motors speed
 	}
 }
-
-
+//------------------------------------------------------------------------------
 /**
-* @param dir: this character takes the values L and R for left and right respectively
-* @method mTurn
-*/
-void mTurn(int dir, int vel){
-	if (readyPWM == false){ //Checks if the motor is accelerating
-		PWM(dir, vel);	//Calls the PWM funciton if the motor is accelerating
-	}
-	else if (readyPWM == true) { //If the motor is warmed up
-		if (dir == left){ //Checks if the direction is left
-			//Sets the speed according to one wheel being backwards
-			setSpeed(m1, -1*vel);
-			setSpeed(m2, vel);
-		}
-		if (dir == right){ //Checks if the direction is right
-			//Sets the speed opposite to the previous statement
-			setSpeed(m1, vel);
-			setSpeed(m2, -1*vel);
-		}
-	}
-}
-/**
-* @param dir: this character takes the values L and R for left and right respectively
-* @method mTurn
+*
+* @method deadEndTurn
 */
 void deadEndTurn(){
 	//Declaration of local variables
@@ -159,12 +178,13 @@ void deadEndTurn(){
 		currSpeed = 0;
 		//Turns the avc just enough so that the left sensor is always less than the wall distance until the turn in complete
 		for (int i = 0; i<turnThreshold; i++){
-            readyPWM = false;
-			mTurn(right, deadSpeed);//Turns right, so the left sensor is always smaller than the wall distance
-		}
+			setSpeed(m1, -1*deadSpeed + i);//Turns right, so the left sensor is always smaller than the wall distance
+            setSpeed(m2, deadSpeed - i); //Slows down as it comes around.
+        }
 		//Continues to turn right until the wall distance is greater than or equal to the left reading.
 		while (dLeft > dWall){
-			mTurn(right, deadSpeed);
+            setSpeed(m1, -1*deadSpeed);//Turns right, so the left sensor is always smaller than the wall distance
+            setSpeed(m2, deadSpeed);
 			if (dLeft <= dWall){
 				mazePID();//Restarts the PID after the turn is complete
 				return; //Exits the loop, when the avc has completed the full 180 degree turn or more.
@@ -172,23 +192,26 @@ void deadEndTurn(){
 		}
 	}
 }
+//------------------------------------------------------------------------------
 /**
 *
 * @method mazePID
 */
 void mazeCheck(){
+  //Takes the readings from both of the IR sensors
 	takeReading(s1);
 	takeReading(s2);
-	if (dLeft == dWall-dWallError && dRight == dWall-dWallError){
-		inMaze = true;
-	}
-	if (dLeft == dWall+dWallError && dRight == dWall+dWallError){
-		inMaze = true;
+  //If the robot is in the maze this condition should be true
+	if (dLeft <= dWall-dWallError || dLeft <= dWall+dWallError){
+        if (dRight <= dWall+dWallError || dRight <= dWall-dWallError){
+    		  inMaze = true;
+        }
 	}
 	else {
 		inMaze = false;
 	}
 }
+//------------------------------------------------------------------------------
 /**
 *
 * @method mazePID
@@ -201,20 +224,21 @@ void mazePID(){
 	float kp = maxSpeed/maxReading;
 	float ki = 0;
 	float kd = 0;
-
+    //PID signal variables
 	float pSig;
 	float iSig;
 	float dSig;
-
+    //Runs while the robot is in the maze
 	while(inMaze){
 		//Takes an error signal of how far away the avc is from the center based of the left wall
 		speedPID = (int) (pSig)*kp;
-		if (pSig != 0){ //Turn if it is not centered
+        //Turn if it is not centered
+		if (pSig != 0){
 			set_motor(m1,speedPID);
 			set_motor(m2,speedPID);
-			//mTurn(dirPID, (pSig)*kd); //Turn accordingly
 		}
-		else if (pSig == 0){ //If it is centered
+        //If it is centered
+		else if (pSig == 0){
 			setSpeed(3, speed); //Keep going forward
 		}
 		//If the readings are both just slightly smaller than the wall distance
@@ -225,15 +249,15 @@ void mazePID(){
 
 	}
 }
-
+//------------------------------------------------------------------------------
 /**
-*
+* @method main
 */
 int main(){
-    InitHardware();
+    InitHardware(0);//Initilize the RPis harware
+    //Check if the avc is in the maze continuosly.
     while(!inMaze){
-	mazeCheck();
-}
+    	mazeCheck(); //Check if it in the maze.
+    }
 	return 0;
 }
-
