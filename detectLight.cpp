4079988eae2bb7a6244358cc03b4b5b8c2f45c23
corbeygh <corbeygh@users.noreply.github.com>
2016-05-14 14:12:17 +1200

    Update detectLight.cpp
    Test code to print the value of error Value for P..
    After we know what Value it is we can adjust kp.

--- detectLight.cpp
+++ detectLight.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 
 //these load specific methods from the ENGR101 library
 extern "C" int init(int d_lev);
@@ -11,26 +12,35 @@ extern "C" int set_motor( int motor , int speed);
 int main(){
     //This sets up the RPi hardware and ensures
     //everything is working correctly
+	//Initializing variables
     init(0);
-    int maxSpeed = 40;//You can change this value from 0-255
-    float kp = 0.5;
-    int z = 320; //Maximum array size
-    int pixCord[z]; //Initializes the array Size
+    int w;
+    int s;
+    double errorValue;
+    //int maxSpeed = 100;
+    //int VL;
+	//int VR;
+    //double kp = 1;
+
 //-----------------------------------------------------------
     while(true){
        take_picture();
-       int s;
-       int w;
-       w  = get_pixel(160,120,3); // Looks at center pixel
-       if(w < 127){ //If pixel is closer to black
-    	   s=0;
-       }else {
-           s=1;
-       }
-        if(s==1){//White light detected:
-        	printf("White \n");
-        } else if(s==0){
-        	printf("Black \n");
-        }
-}//Closers Main Loop
+       errorValue = 0;
+       for (int i = 0; i < 32; i++){
+    	   w = get_pixel(i*10,120,3);
+    	   if(w < 127){ //If pixel is close to black
+    		   s=0;
+    	   }else {
+    		   s=1;
+    	   }
+    	   errorValue = errorValue + (i-160)*s; //Adds to errorValue if its a white pixel
+       }//Closes For Loop
+
+       errorValue = round(errorValue); //Rounds errorValue to a whole number
+       printf("Error Value: %d" ,errorValue);
+       //VL = maxSpeed - (errorValue*kp);
+       //VR = maxSpeed + (errorValue*kp);
+       //set_motor(1,VL);
+       //set_motor(2,-VR);
+    }//Closes While Loop
 return 0;}
