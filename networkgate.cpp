c3b4808c764ef921d282a65c9a4f7ec427f018db
James Magallanes <jamesmagallanes@gmail.com>
2016-05-18 14:27:18 +1200

    THIS WORKS. removed unnecessary lines and changed some

--- networkgate.cpp
+++ networkgate.cpp
@@ -11,11 +11,11 @@ void networkGate();
 
 void networkGate(){
     init(1);		//initialises rPi
-    char message[24];
-    connect_to_server("130.195.6.196", 1024);		//connects to server with the ip address 192.168.1.2 //130.195.6.196
-    send_to_server("Please");		//sends a message to the connected server
-    receive_from_server(message); //this may be buggy! //receives message from the connected server
-    send_to_server(message);		//sends a message to the connected server
+    char message[24]; //initialises array of chars, size 24
+    connect_to_server("130.195.6.196", 1024);		//connects to server @130.195.6.196:1024
+    send_to_server("Please");		//sends "Please" to server
+    receive_from_server(message); //receives message
+    send_to_server(message);	//sends back received message
 }
 
 int main(){
