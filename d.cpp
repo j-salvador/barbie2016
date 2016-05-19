2fbaccc869e298e541fc772de5652c0bc30ee059
corbeygh <corbeygh@users.noreply.github.com>
2016-05-19 22:03:20 +1200

    Update d.cpp
    D is dependent Changes between Frame to frame

--- d.cpp
+++ d.cpp
@@ -16,7 +16,7 @@ int main(){
     init(0);
     int w;
     int s;
-    double P = 0;
+    double P = 5;
     double D = 0;
     double kp = 0.0020;
     double kd = 0;
@@ -35,48 +35,56 @@ int main(){
     double delta_error;
 //-----------------------------------------------------------
     while(true){
-    	start = tim(NULL);
+    	start = tim(NULL);//Starts Timer
     	take_picture();
-    	previous_error = avgC;
+//--------------------------------------Analyzing Image--------------------------------
     	for (int i = 0; i < 320; i++){
     	   w = get_pixel(i,120,3);
-    	   if(w < 127){ //If pixel is close to black
+    	   if(w < 127){ //If pixel is close to black ------ (Reduces noise)
     		   s=0;
     	   }else {
     		   s=1;
     	   }
     	   current_error = current_error + (i-160)*s; //Adds to current_error if its a white pixel
     	}//Closes For Loop
-    	avgC = current_error/320
-    	//current_error = round(current_error); //Rounds current_error to a whole number
-    	P = current_error*kp;
-    	
-    			
-    	//printf("Error Value: %d \n" ,P);
-    	VL = maxSpeed - (P);// - (D);
-    	VR = maxSpeed + (P);// + (D);
-    	//set_motor(1,VL);
-    	//set_motor(2,-VR);
-    	//Time Stamp:
+//--------------------------------------Time Stamp & FPS---------------------------
     	finish = time(NULL);
     	dif = finish -start;
-    	if(dif==1){
-    		tim = 1/count;
-    		total_error = total_error/count; //Averages error by current FPS
-    		D = ((total_error-previous_error)/1)*kd;
-    		previous_error = total_error;
-    		
-    		printf("FPS = %d \nVL = %d\nVR = %d\nCurrent error = %f\nPrevious error= %f",count,VL,VR,avgC,previous_error);    		
+    	if(dif==1){// Once happens when 1 second has passed.
+    		tim = 1/count; //Updates the current time taken for AVC to process 1 Frame
     		count = 0;
-    		total_error = 0;
     	}
-    	total_error = total_error +current_error;
-    	current_error = 0;   			
+//---------------------------------------Error Values-------------------------------
+    	avgC = current_error/320;
+    	P = avgC*kp;
+		D = ((avgC-previous_error)/tim)*kd;
+		printf("D = %f\nP = %f\n",D,P); //Debugging
+    	
+		//Initializing values for next frame
+		previous_error = avgC;
+    	current_error = 0; //Resets current error to 0
+		avgC = 0;
     	count++;
+//----------------------------------------Motor Control--------------------------
+      	VL = maxSpeed - (P);// - (D);
+    	VR = maxSpeed + (P);// + (D);
+    	set_motor(1,VL);
+    	set_motor(2,-VR);    	
+    	
     }//Closes While Loop
 return 0;}
 /*
- * 
- * 
- * 
+ * If the line is to the Left then the Current Error value should be positive.
+ * Add if statement for timer if(start){start = time(NULL);}
+ *
+*/
+/*if(timer){
+ *	start = time(NULL);
+ *	timer = false;
+ *}
+ *cycles through main while loop
+ *finish = time(NULL); // Finish time gets larger and larger until it is 1 second more than start
+ *if(dif==1){
+ *	timer = true; //Restarts clock
+ *}
 */
