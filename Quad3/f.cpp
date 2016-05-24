9d4235255a3b186b8cb6c33697195d1af03f1891
corbeygh <corbeygh@users.noreply.github.com>
2016-05-25 11:07:43 +1200

    Will it make it to the maze?
    Lets find out

--- Quad3/f.cpp
+++ Quad3/f.cpp
@@ -18,9 +18,9 @@ int main(){
     int s;
     double P = 0;
     double D = 0;
-    double kp = 0.31;
+    double kp = 0.27;
     double kd = 0.003;
-    int maxSpeed = 30;
+    int maxSpeed = 40;
     int VL;
     int VR;
     time_t start; //Sets 0 Point for timer to begin the clock at.
@@ -33,21 +33,21 @@ int main(){
     double previous_error = 0;
     double delta_error;
     int setTime = 0;
+
     bool line = true;
     int iCount = 0;
     double lastCurrent_error = 0;
-
-//----------- Quad 3 Field ------------
-    bool left = false;
-    bool right = false;
-    bool middle = false;
-    int spike = 10000; //Not sure what this is.
-    bool deadEnd = false;
-    double tIntersection = 999999; //Have no idea what this value is either.
+    bool white_light;
+    int l = 0;
+    int r = 0;
+    int turn_180 = 0;
 //------------------------------------------------------------------------------
     while(true){
     	start = time(NULL);//Starts Timer
     	take_picture();
+      white_light = true;
+      l = 0;
+      r = 0;
 //--------------------------------------Analyzing Image-------------------------
     	for (int i = 0; i < 320; i++){
     	   w = get_pixel(i,120,3);
@@ -55,82 +55,99 @@ int main(){
     		   s=0;
     	   }else {
     		   s=1;
-    	   }
+           white_light = false;
+           if(i<160){
+             l++;
+           }else{
+             r++;
+           }
+         }
     	   current_error = current_error + (i-160)*s; //Adds to current_error if its a white pixel
     	}//Closes For Loop
+
+//-------------------------------------No Line Detected-------------------------
+        if(l >100 || r >100){//Spin around 180}
+
+//--------------Debugging-------------------------
+if(l >100 || r >100){
+        printf("Left = %d\nRight = %d\n"l,r)//Debugging
+
+      printf("Turning Around %d\n",turn_180);
+    }
+      //VL = maxSpeed;
+      //VR = maxSpeed;
+      //set_motor(1,VL);
+      //set_motor(2,VR);
+      //Sleep(0,100000);
+      //turn_180++;
+//--------------Debugging ENDS-------------------------
+
+
+        if(current_error == 0 && white_light){ //error is 0 and black_light
+          line = false;
+          VL = maxSpeed;
+          VR = maxSpeed;
+          set_motor(1,-VL);//Sets motors to reverse
+        	set_motor(2,VR);
+          iCount++; //Counts number of frames line has been missing for
+        }else{
+          line = true;
+          iCount = 0;
+        }
+        if(iCount>0){ //Debugging
+          printf("Count = %d (line has been missing for %d FPS)\n",iCount,iCount);
+        }
+          if( current_error<0 || current_error>0 ){
+              lastCurrent_error = current_error;
+          }
+          if(iCount>7){ //Line has been lost for a while. Run Correction
+              if(lastCurrent_error>0){
+                set_motor(1,-VL);
+              	set_motor(2,-VR);
+                Sleep(0,100000);
+              }else if(lastCurrent_error<0){
+                set_motor(1,VL);
+              	set_motor(2,VR);
+                Sleep(0,100000);
+              }
+          }//Closes iCount if statement
+
+//--------------------------------------Time Stamp & FPS------------------------
+      finish = time(NULL);
+    	dif = finish -start;
+    	if(dif==1){// Only occurs when 1 second has passed when 1 second has passed.
+    		    tim = 1/count; //Updates the current time taken for AVC to process 1 Frame
+    		    count = 0;
+    	}
 //---------------------------------------Error Values---------------------------
-    	avgC =  (current_error/320);
+    	avgC = avgC + (current_error/320);
     	P = (current_error/320)*kp;
 		  //printf("D = %f\nP = %f\n",D,P); //Debugging
-//-------------------------------- Quad 3 Detect Line changes ------------------
+    	current_error = 0; //Resets current error to 0
+		  setTime++;
+      count++;
+//----------------------------- D value for PID --------------------------------
+      if(setTime == 1){ //This means our D value changes every frame
+
+       	D = ((avgC-previous_error)/(0.03))*kd;
+        previous_error = avgC;
+    		avgC = 0;
+    		setTime = 0;
+    	}
+//----------------------------------------Motor Control-------------------------
+    if(line){
 
 
-      printf("Spike = %f\navg = %f\nC",current_error,avgC);
-      if(current_error< -1*spike ){
-        left = true; //This is an Intersection with left exit path
-      }
-      if(current_error > spike){
-         right = true;//Right exit path
-      }
-      if(current_error > tIntersection){ //Not sure what t Intersection looks like
-        middle = true; //T Intersection, right and left exit
-      }
-      if(current_error=0){
-        deadEnd = true; //needs to turn around
-      }
-      if(P == 0){ //detect its ontop of the line
-        halt = true; //Purpose to See if its done a correct turn. If so this will change this to go forward
-        left = false;
-        initialLeft = true;
-        right = false;
-        deadEnd = false;
-
-      }
-//---------------------------------- Pulse to begin turn-------------------------
-//This is going to be really different from quad2
-//Prioritizes turning left
-//Method Pulse in direction then let P take over.
-
-if(left){
-//  printf("Left\n");
-    VL = maxSpeed;
-    VR = maxSpeed;
-    set_motor(1,-VL);
-    set_motor(2,-VR);
-    Sleep(0,100000);
-    left = false;
-
-}else if(right){ //-----------------------Right-------------------------------
-//  printf("Right\n");
-  //Currently turning right ----Same as left but differernt motor direction
-  VL = maxSpeed;
-  VR = maxSpeed;
-  set_motor(1,VL);
-  set_motor(2,VR);
-  Sleep(0,100000);
-  right = false;
-
-}else if(deadEnd){ //-----------------------Dead End----------------------------
-//  printf("deadEnd\n");
-  //Turns right
-  VL = maxSpeed;
-  VR = maxSpeed;
-  set_motor(1,VL);
-  set_motor(2,VR);
-  Sleep(0,500000);
-  deadEnd = false;
-}
-//------------------------------------------------------------------------------
-//-------------------------------- Main Motor Control --------------------------
-VL = maxSpeed - (P);
-VR = maxSpeed + (P);
-set_motor(1,VL);
-set_motor(2,-VR);
-current_error = 0; //Resets current error to 0
+      VL = maxSpeed - (P) + (D);
+    	VR = maxSpeed + (P) - (D);
+    	set_motor(1,VL);
+    	set_motor(2,-VR);
+    }
 //-----------------------------------------END OF PROGRAM-----------------------
 }//Closes While Loop
 return 0;}
-/*Notes
- *
- *
+/*
+ * Kp & Kd values could be tuned more finely if time persists
+ * Need to add: D = round(D); & P = round(P);
+ *Time Stamp needs fixing
 */
