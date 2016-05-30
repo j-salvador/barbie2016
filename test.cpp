0b22576442a68f6184eca20d727cf02364798d61
corbeygh <corbeygh@users.noreply.github.com>
2016-05-30 16:47:58 +1200

    test gate+quad2+quad3

---
+++ test.cpp
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+//these load specific methods from the ENGR101 library
+extern "C" int init(int d_lev);
+extern "C" int connect_to_server( char server_addr[15],int port);
+extern "C" int send_to_server(char message[24]);
+extern "C" int receive_from_server(char message[24]);
+extern "C" int take_picture();
+extern "C" char get_pixel(int row,int col,int color);
+extern "C" int Sleep(int sec, int usec);
+extern "C" int set_motor( int motor , int speed);
+
+int main(){
+  init(0);
+  int r = 0;
+  int l = 0;
+  double current_error = 0;
+  double lastCurrent_error = 0;
+  double horizontal_error = 0;
+  int s;
+  int top;
+  int bottom;
+  bool quad2 = false;
+  bool quad3 = false;
+  int maxSpeed = 35;
+  int VL;
+  int VR;
+  int iCount = 0;
+  bool gate = true;
+  bool followLine = false;
+  bool maze = false;
+
+  double P = 0;
+  double kp = 0.27;
+  double avgC = 0;
+  bool line = true;
+  while(gate){
+    //gate code to open gate
+    init(1);		//initialises rPi
+    char message[24]; //initialises array of chars, size 24
+    connect_to_server("130.195.6.196", 1024);		//connects to server @130.195.6.196:1024
+    send_to_server("Please");		//sends "Please" to server
+    receive_from_server(message); //receives message
+    send_to_server(message);	//sends back received message
+
+    //once gate is open:
+    gate = false;
+    followLine = true;
+  }
+  while(followLine){ // Main Loop ---To follow line
+    init(0);
+    take_picture();
+    l = 0;
+    r = 0;
+    top = 0;
+    bottom = 0;
+    current_error = 0;
+    horizontal_error = 0;
+    Correction = true;
+
+    //--------------------------------------Analyzing Image-------------------------
+        	for (int i = 0; i < 320; i++){
+        	   int row = get_pixel(i,120,3);
+        	   if(row < 127){ //If pixel is close to black ------ (Reduces noise)
+        		   s=0;
+        	   }else {
+        		   s=1;
+               Correction = false;
+               if(i<160){
+                 l++;
+               }else{
+                 r++;
+               }
+             }
+        	   current_error = current_error + (i-160)*s; //Adds to current_error if its a white pixel
+        	}//Closes For Loop
+          //------Scans 2nd 1.
+          for (int i = 0; i < 240; i++){
+
+        	   int col = get_pixel(180,i,3);
+        	   if(col < 127){ //If pixel is close to black ------ (Reduces noise)
+        		   int s = 0;
+        	   }else {
+        		   int s = 1;
+               if(i<120){
+                top++;        //line
+              }else{
+                bottom++;
+              }
+             }
+        	   horizontal_error = horizontal_error + (i-120)*s; //Adds to current_error if its a white pixel
+        	}//Closes For Loop
+
+//--------------------------------Analyzing Image Finish------------------------
+
+if(quad2){
+  //quad2 error correction if it loses the line
+
+  //I've added under T intersection, left corner and right corner that
+  //quad 2 switchs to false if one of those are detected.
+if(current_error == 0 && Correction){
+  iCount++;
+}else{
+  iCount = 0;
+}
+
+  if(iCount>0){ //Debugging
+    printf("Count = %d (line has been missing for %d FPS)\n",iCount,iCount);
+  }
+    if( current_error<0 || current_error>0 ){
+        lastCurrent_error = current_error;
+    }
+    if(iCount>7){ //Line has been lost for a while. Run Correction
+      set_motor(1,-VL);
+      set_motor(2,VR);
+      Sleep(0,180000);
+
+      if(current_error == 0){
+        VL = maxSpeed;
+        VR = maxSpeed;
+        if(lastCurrent_error>0){
+          set_motor(1,-VL);
+          set_motor(2,-VR);
+          Sleep(0,050000);
+
+        }else if(lastCurrent_error<0){
+          set_motor(1,VL);
+          set_motor(2,VR);
+          Sleep(0,050000);
+
+        }
+      }
+    }//Closes iCount if statement
+}
+//-------------------------------Quad 3 Trigger---------------------------------
+if(l >155 && r >155 && bottom >115){ //T Intersection
+  //Choose left path
+  //turn left
+
+  quad2 = false;
+  quad3 = true;
+}else if(l >155 && bottom >115){ //Left Corner
+  //turn left by fixed amount
+  set_motor(1,-VL);
+  set_motor(2,-VR);
+  Sleep(0,200000);
+  quad2 = false;
+  quad3 = true;
+}else if(r >155 && bottom >115){ //Right Corner
+  //turn right by fixed amount
+  set_motor(1,VL);
+  set_motor(2,VR);
+  Sleep(0,200000);
+  quad2 = false;
+  quad3 = true;
+}else if(quad3 && current_error == 0){ //deadEnd
+//Turns 180* degrees if it loses the line and is in quad3
+  set_motor(1,VL);
+  set_motor(2,VR);
+  Sleep(0,400000);
+}else{ //goes straight if none Detected
+  line = true;
+}
+//-------------------------------Quad 3 Trigger FINSIH--------------------------
+
+//---------------------------------------Error Values---------------------------
+    	avgC = avgC + (current_error/320);
+    	P = (current_error/320)*kp;
+//------------------------------------------------------------------------------
+
+
+
+//----------------------------------------Motor Control------------------------
+if(line){
+  VL = maxSpeed - (P);
+  VR = maxSpeed + (P);
+  set_motor(1,VL);
+  set_motor(2,-VR);
+}
+//--------------------------------------Detects Maze--------------------------
+if(maze){ //If we reach maze this will be used to switch to maze while loop
+  followLine = false
+  maze = true;
+}
+
+//------------------------------------------------------------------------------
+        }//---Closes Main While Loop
+while(maze){
+  //solve maze easy... :D
+}
+
+      }//Closes Int Main
